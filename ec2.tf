01f170f60804dd4616d7addd7ececc92eeb825aa
rushikeshkulat95 <rushikeshkulat95@gmail.com>
2023-02-25 12:10:43 +0530

    access and scret key changed

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAZDYDAMMNP3A2Q3GX"
-  secret_key = "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"
+  access_key = "AKIAZDYDAMMNKJFXMDJG"
+  secret_key = "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"
 }
 
 
-resource "aws_instance" "demo" {
+resource "aws_instance" "demo1" {
   ami           = "ami-0e742cca61fb65051"
   instance_type = "t2.micro"
 
